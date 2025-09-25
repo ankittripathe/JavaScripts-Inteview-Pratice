a07967b40b5998711792b62f721f150de5d378a8
Ankit Tripathi <ankittripathi.dev@gmail.com>
2025-09-25 17:05:50 +0530

    done

--- 01_function.js
+++ 01_function.js
@@ -1,6 +1,6 @@
 /* 
 (1) Give a string, reverse each word in the sentence. 
-"Harsh bhai kaise ho"
+"Ankit bhai kaise ho"
 */
 let str = "Hello ji kaise ho"
 let reverseStr = ''
@@ -11,12 +11,13 @@ for(let i = str.length-1; i>=0; i--){
 console.log(reverseStr);   // output: oh esiak ij olleH
 
 
-// Method 2nd => map return new array
+// Method-1:- map always return new array
 let myStr = 'Ankit kaise ho'
 let savedStr = myStr.split(" ").map((word)=> {
     return word.split("").reverse().join("")
 })
 console.log(savedStr.join(" "));
+
 
 
 // (2) How to check if an object is array or not ? Provide some code.
@@ -29,6 +30,7 @@ console.log(checkArray({}));  // output: false
 /*
 Notes: retrun ka matlab hota hai oha pe wapas jawo jaha ke wajah se function chala hai.
 */
+
 
 // (3) How to empty an array in javaScripts ? do not reset it to a new array, and do not loop through to pop each value
 // method-1
@@ -47,10 +49,10 @@ for(let  i = array.length-1; i>=0; i-- ){
 console.log('finalArray =', array);
 
 
-// (4) How to check if a number is an integer ?
-/*
-Integers value => zero, positive, negative
-float value => fractional value, decimal value
+/* 
+(4) How to check if a number is an integer ?
+ Integers value => zero, positive, negative
+ float value => fractional value, decimal value
 */
 const checkNumber = (num)=>{
     return Number.isInteger(num)
@@ -90,13 +92,9 @@ console.log(answer);
 // (6) Write a javaScript function that reverse a number
 function reverseKaro(num){
     let str = num.toString()
-
     let arrayStr = str.split('')
-
     let reverseStr = arrayStr.reverse()
-
-    let  finalReverse = reverseStr.join("")
-
+    let finalReverse = reverseStr.join("")
     console.log(typeof finalReverse);
 
     let finalReverseInNumber = Number(finalReverse)
@@ -118,7 +116,6 @@ let reverseNumber = (num)=>{
     return rev
 }
 console.log(reverseNumber(1234));
-
 
 
 // (7) Write a javaScript function that checks wheather a passed string is palidrome or not
@@ -162,8 +159,25 @@ function capitalizeKaro(str){
 console.log(capitalizeKaro('ankit bhai kaise ho'));
 
 
+
 /*
-(10) Write a JavaScripts function to get the number of occurrences of each letter in specified string
+(10) Write a JavaScripts function which accepts an argumnets and returns the type.
+Note: There are six possible values that typeof returns: object, boolean, function, number, string, undefined.
+*/
+function typeTeller(arg){
+    console.log(typeof arg);
+}
+typeTeller([]);            // object
+typeTeller(12)             // number
+typeTeller('12')           // string
+typeTeller(true);          // boolean
+typeTeller();              // undefined
+typeTeller(undefined)      // undefined
+typeTeller(function (){})  // function
+
+
+/*
+(11) Write a JavaScripts function to get the number of occurrences of each letter in specified string
 Example: apple => a=1, p=2, l=1, e=1
 */
 function occurrences(str){
@@ -180,3 +194,6 @@ function occurrences(str){
 }
 console.log(occurrences('apple'));
 console.log(occurrences('shivajitheboss'));
+
+
+
